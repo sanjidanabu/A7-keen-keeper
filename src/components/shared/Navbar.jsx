a48c08fffb3ddfdb30a184259a425243f963208c
sanjidanabu <sanjidanabiha@gmail.com>
2026-04-14 14:02:31 +0600

    create nav ui

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from '../../assets/logo.png'
+import { HiOutlineHome } from 'react-icons/hi';
+import { MdOutlineAccessTime } from 'react-icons/md';
+import { IoStatsChartOutline } from 'react-icons/io5';
+
+const Navbar = () => {
+    return (
+         <nav className="bg-white border-b border-gray-100 px-4 py-3 md:px-10">
+            <div className="max-w-7xl mx-auto flex justify-between items-center">
+                
+             
+                <div className="flex items-center">
+                    <img src={logo} alt="KeenKeeper" className="h-8 w-auto object-contain" />
+                </div>
+
+                
+                <div className="flex items-center space-x-6">
+                    
+                  
+                    <button className="flex items-center gap-2 text-gray-500 hover:text-gray-800  px-4 py-2 rounded-lg font-medium transition-all">
+                        <HiOutlineHome className="text-xl" />
+                        <span className="hidden sm:inline">Home</span>
+                    </button>
+
+                  
+                    <button className="flex items-center gap-2 text-gray-500 hover:text-gray-800 transition-colors font-medium">
+                        <MdOutlineAccessTime className="text-xl" />
+                        <span className="hidden sm:inline">Timeline</span>
+                    </button>
+
+                 
+                    <button className="flex items-center gap-2 text-gray-500 hover:text-gray-800 transition-colors font-medium">
+                        <IoStatsChartOutline className="text-xl" />
+                        <span className="hidden sm:inline">Status</span>
+                    </button>
+
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router/dom'
+import { createBrowserRouter } from 'react-router'
+import RootLayout from './layout/RootLayout'
+import HomePage from './pages/homepage/HomePage'
+
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element: <RootLayout/>,
+      children:[
+        {
+          path:"/",
+          element: <HomePage/>
+        },
+        {
+          path: "/timeline",
+          element: <h2>timeline</h2>
+        },
+      ],
+
+   errorElement: <h2>this page is not found</h2>,
+    
+    
+    },
+
+   
+  ]
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <h1 className='bg-amber-300'>welcome home</h1>
+   
+    <RouterProvider router={router} />
   </StrictMode>,
 )
